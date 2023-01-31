291885ef2c45e44dc5a7703b613bb70a89f670bd
NL <law.nathaniel8@gmail.com>
2023-01-31 15:32:24 -0800

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Nathaniel Law on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
