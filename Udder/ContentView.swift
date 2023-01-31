4f07f32752c32930bc18e9777990897c2f34da93
NL <law.nathaniel8@gmail.com>
2023-01-31 15:35:35 -0800

    Add label

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Udder")
+                .font(.title)
+            Text("Fresh Milk, On Demand")
         }
         .padding()
     }
